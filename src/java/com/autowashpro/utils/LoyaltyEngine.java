2a3543b37b06c44b4fe75fbbf8a3ad65e989ae58
nthanhduy310@gmail.com <nthanhduy310@gmail.com>
2026-05-20 14:51:31 +0700

    feat: implement LoyaltyEngine and corresponding unit tests; add CI workflow for automated testing

---
+++ src/java/com/autowashpro/utils/LoyaltyEngine.java
@@ -0,0 +1,8 @@
+package com.autowashpro.utils;
+
+public class LoyaltyEngine {
+    // Quy tắc: 1 điểm = 1000 VND
+    public static int calculatePoints(double spendAmount) {
+        return (int) (spendAmount / 1000);
+    }
+}

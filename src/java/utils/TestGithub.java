965c1c50b172319159c53b9d3ce985b5ed02a47e
Duykhobo <126856290+Duykhobo@users.noreply.github.com>
2026-05-21 15:22:41 +0700

    feat: test method để push

---
+++ src/java/utils/TestGithub.java
@@ -0,0 +1,22 @@
+package utils;
+
+public class TestGithub {
+    private String id;
+
+    public TestGithub() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "TestGithub [id=" + id + "]";
+    }
+
+}
